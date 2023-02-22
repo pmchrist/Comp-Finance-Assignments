7c1ac31a67bd7ddcf052be3ffbd0365ed31b7d3e
pmchrist <pmchrist@protonmail.com>
2023-02-22 15:50:47 +0100

    Organized euler function to take custom inputs

--- Assignment_1/part3.py
+++ Assignment_1/part3.py
@@ -3,27 +3,21 @@ import matplotlib.pyplot as plt
 from math import *
 from scipy.stats import norm
 
+# Option pricing functions
 def d1_d2(S0, K, r, sigma, T):
     d1 = (log(S0/K) + (r+sigma**2/2)*T)/(sigma*sqrt(T))
     d2 = d1 - sigma*sqrt(T)
     return d1,d2
 def call_option_price(S0, K, r, T, d1, d2):
     return S0*norm.cdf(d1) - K*exp(-r*T)*norm.cdf(d2)
-
 def put_option_price(S0, K, r, T, d1,d2):
     return K*exp(-r*T)*norm.cdf(-d2) - S0*norm.cdf(-d1)
-maturity = 365
-S0 = 100
-strike = 99
-K = 100
-volatility_delta = 0.2
-volatility_stock = 0.2
-r = 0.06
-hedge_frequency = 7
 
-
-
-def GBM_euler_mthod(S0, maturity, hedging_frequency):
+# Hedging function
+def GBM_euler_mthod(S0, K, r, volatility_delta, volatility_stock, maturity, hedging_frequency):
+    """
+    Maturity is in days
+    """
     all_stock_prices = []
     all_call_option_prices = []
     all_call_options_hedged_daily = []
@@ -56,19 +50,24 @@ def GBM_euler_mthod(S0, maturity, hedging_frequency):
         all_daily_deltas.append(normal_d1_daily)
     return all_stock_prices, all_call_option_prices, all_call_options_hedged_daily, all_put_option_prices, all_deltas, all_daily_deltas
 
-
-
+# Plotting function
 def plotting_func(x,y, label = None):
     if label:
         plt.plot(x,y, label = label)
     else:
-
         plt.plot(x,y)
 
-
+# Default option parameters
+maturity = 365
+S0 = 100
+K = 99      # Strike price
+volatility_delta = 0.2
+volatility_stock = 0.2
+r = 0.06
+hedge_frequency = 7
 
 if __name__ == "__main__":
-    stock_prices, call_option_prices, all_call_options_hedged_daily, put_option_prices, deltas, all_daily_deltas = GBM_euler_mthod(S0, maturity, hedge_frequency)
+    stock_prices, call_option_prices, all_call_options_hedged_daily, put_option_prices, deltas, all_daily_deltas = GBM_euler_mthod(S0, K, r, volatility_delta, volatility_stock, maturity, hedge_frequency)
     deltas = [(all_daily_deltas, f'Delta Hedged Daily'),(deltas, f'Delta Hedged Every {hedge_frequency} days')]
     plotted_prices = [(stock_prices, 'Stock Price'), (call_option_prices, 'Option Price Hedged Daily'), (all_call_options_hedged_daily, f'Option Price Hedged Every {hedge_frequency} days')]#, (put_option_prices, 'Put Option Price') ]
     for i in plotted_prices:
